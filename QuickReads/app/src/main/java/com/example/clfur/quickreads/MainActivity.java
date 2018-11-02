4cda8fa6ac051582e0ee9ddb6de67f0935baed5a
RollickBoon <dnsnwb@mst.edu>
2018-11-01 21:35:02 -0500

    working nav bar

--- QuickReads/app/src/main/java/com/example/clfur/quickreads/MainActivity.java
+++ QuickReads/app/src/main/java/com/example/clfur/quickreads/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.clfur.quickreads;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Fragment mHomeFragment;
+    private Fragment mDashboardFragment;
+    private Fragment mNotificationFragment;
+    private FrameLayout mMainFrame;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,12 +26,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    setFragment(mHomeFragment);
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
+                    setFragment(mDashboardFragment);
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
+                    setFragment(mNotificationFragment);
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -37,15 +48,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mHomeFragment = new HomeFragment();
+        mDashboardFragment = new DashboardFragment();
+        mNotificationFragment = new NotificationFragment();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        setFragment(mHomeFragment);
+    }
 
-
-
-
-
-
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 
 }
